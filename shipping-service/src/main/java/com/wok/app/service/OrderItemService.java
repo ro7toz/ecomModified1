322aaf9d297b289faea97633787a280fad47319f
ro7toz <tushkinit@gmail.com>
2025-12-09 21:56:33 +0530

    Update OrderItemService.java

--- shipping-service/src/main/java/com/wok/app/service/OrderItemService.java
+++ shipping-service/src/main/java/com/wok/app/service/OrderItemService.java
@@ -2,11 +2,12 @@ package com.wok.app.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;  // ADD THIS IMPORT
+
 import com.wok.app.domain.id.OrderItemId;
 import com.wok.app.dto.OrderItemDto;
 
-@Transactional
-public interface OrderItemService {
+public interface OrderItemService {  // REMOVE @Transactional from interface
 	
 	List<OrderItemDto> findAll();
 	OrderItemDto findById(final OrderItemId orderItemId);
