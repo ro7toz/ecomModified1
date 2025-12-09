050e575dad5465b481ddc08e368bf5468825d6dd
ro7toz <tushkinit@gmail.com>
2025-12-09 15:54:37 +0530

    change 5

--- shipping-service/src/main/java/com/wok/app/service/impl/OrderItemServiceImpl.java
+++ shipping-service/src/main/java/com/wok/app/service/impl/OrderItemServiceImpl.java
@@ -50,16 +50,16 @@ public class OrderItemServiceImpl implements OrderItemService {
 	@Override
 	public OrderItemDto findById(final OrderItemId orderItemId) {
 		log.info("*** OrderItemDto, service; fetch orderItem by id *");
-		return this.orderItemRepository.findById(null)
-				.map(OrderItemMappingHelper::map)
-				.map(o -> {
-					o.setProductDto(this.restTemplate.getForObject(AppConstant.DiscoveredDomainsApi
-							.PRODUCT_SERVICE_API_URL + "/" + o.getProductDto().getProductId(), ProductDto.class));
-					o.setOrderDto(this.restTemplate.getForObject(AppConstant.DiscoveredDomainsApi
-							.ORDER_SERVICE_API_URL + "/" + o.getOrderDto().getOrderId(), OrderDto.class));
-					return o;
+		return this.orderItemRepository.findById(orderItemId)
+			.map(OrderItemMappingHelper::map)
+			.map(o -> {
+				o.setProductDto(this.restTemplate.getForObject(AppConstant.DiscoveredDomainsApi
+															   .PRODUCT_SERVICE_API_URL + "/" + o.getProductDto().getProductId(), ProductDto.class));
+				o.setOrderDto(this.restTemplate.getForObject(AppConstant.DiscoveredDomainsApi
+															 .ORDER_SERVICE_API_URL + "/" + o.getOrderDto().getOrderId(), OrderDto.class));
+				return o;
 				})
-				.orElseThrow(() -> new OrderItemNotFoundException(String.format("OrderItem with id: %s not found", orderItemId)));
+			.orElseThrow(() -> new OrderItemNotFoundException(String.format("OrderItem with id: %s not found", orderItemId)));
 	}
 	
 	@Override
