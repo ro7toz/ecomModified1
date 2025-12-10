44abf90f822e925325ec7b336ff059aa9d38ea20
ro7toz <tushkinit@gmail.com>
2025-12-10 11:14:01 +0530

    Update CartDto.java

--- order-service/src/main/java/com/wok/app/dto/CartDto.java
+++ order-service/src/main/java/com/wok/app/dto/CartDto.java
@@ -3,6 +3,7 @@ package com.wok.app.dto;
 import java.io.Serializable;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -16,6 +17,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)  // ADD THIS
 public class CartDto implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -24,20 +26,10 @@ public class CartDto implements Serializable {
 	private Integer userId;
 	
 	@JsonInclude(Include.NON_NULL)
-	private Set<OrderDto> orderDtos;
+	@JsonIgnoreProperties("cartDto")  // ADD THIS to prevent circular reference
+	private Set orderDtos;
 	
 	@JsonProperty("user")
 	@JsonInclude(Include.NON_NULL)
 	private UserDto userDto;
-	
 }
-
-
-
-
-
-
-
-
-
-
