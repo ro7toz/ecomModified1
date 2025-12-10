eb6fbefdf7ae6bc8f067ef166430b0347902ef92
ro7toz <tushkinit@gmail.com>
2025-12-10 13:25:02 +0530

    Update OrderNotificationConsumer.java

--- notification-service/src/main/java/com/wok/app/kafka/OrderNotificationConsumer.java
+++ notification-service/src/main/java/com/wok/app/kafka/OrderNotificationConsumer.java
@@ -2,6 +2,7 @@ package com.wok.app.kafka;
 
 import com.wok.app.event.OrderNotificationEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
